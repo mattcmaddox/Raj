5beae23de85c2bcb5605c8938f97e5a83fe2b0b8
mattcmaddox <mattcmaddox@gmail.com>
2012-10-24 22:22:15 -0400

    the real commit for efficiency values

--- raj.py
+++ raj.py
@@ -1,22 +1,20 @@
+#! /usr/bin/python
+import sys
 import random
+import argparse
 from random import randint
 
 
-number_of_humans = 1
-number_of_computers = 5
-total_players = number_of_humans + number_of_computers
-list_of_players = range(total_players)
-
-
-# Creates a hand (set) for every player)
-def create_hands(players):
-    hands = []
-    for player in players:
-        hands.append(set([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]))
-    return hands
-
-players_hands = create_hands(list_of_players)
-
+# Builds Command Line Argument Parser
+def build_parser():
+    parser = argparse.ArgumentParser(description='Command Line Implementation of the Game Raj', add_help=False)
+    parser.add_argument('-h', '--humans', type=int, default=1, help='Number of Human Players')
+    parser.add_argument('-c', '--computers', type=int, default=5, help='Number of Computer Players')
+    parser.add_argument('humans', type=int, nargs='?', default=1, help='Positionial Argument for Number of Humans')
+    parser.add_argument('computers', type=int, nargs='?', default=5, help='Positionial Argument for Number of Computers')
+    parser.add_argument('--help', action='store_true')
+    args = vars(parser.parse_args())
+    return args
 
 # Sets players scores to zero
 def create_scores(players):
@@ -25,18 +23,18 @@ def create_scores(players):
         scores.append(0)
     return scores
 
-scores = create_scores(list_of_players)
+# Creates a hand (set) for every player)
+def create_hands(players):
+    hands = []
+    for player in players:
+        hands.append(set(range(1, 16)))
+    return hands
 
-# Creates a set of treasures valued -5-10 
-treasure_deck = set([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5 ,6, 7, 8, 9, 10])
-
-last_round_treasure = None
-last_round_winner = 0
-
-#^^^^^^^^  happens only at the beginning of game  ^^^^^^^^^^^#
-#||||||||                                         |||||||||||
-
-
+def remove_cards_from_hands(hands, bids):
+    players = range(len(bids))
+    for player in players:
+        hands[player].remove(bids[player])
+    return hands
 
 # Random pick of a treasure from treasure_deck and removes it from the deck
 def draw_treasure(treasure_deck, last_round_winner, last_round_treasure):
@@ -47,122 +45,12 @@ def draw_treasure(treasure_deck, last_round_winner, last_round_treasure):
         return combined_treasure
     return treasure_drawn
 
-
-# Human input
-def human_input(hand, treasure):
-    bid = 0
-    while bid not in hand:
-        print "\nThe treasure is: *%r*" % treasure
-        print "Choose from ", hand
-        bid = raw_input("> ")
-        try:
-            bid = int(bid)
-        except:
-            print "Words are not numbers!"
-    return bid
-
-
-
-# Finds the closest card in hand - Defaults to return the lower value if equal distance 
-# **might not be true anymore**
-def find_closest_card(hand, target_bid):
-    for diff in range(40):
-        bid = target_bid + diff
-        if bid in hand:
-            return bid
-        bid = target_bid - diff
-        if bid in hand:
-            return bid
-    print "ERROR 23: Bid out of range!"
-
-
-
-# Bidding Method 1 - Bid about half the value of the treasure
-def bid_low(treasure, hand):
-    if treasure < 2:
-        treasure = 2
-    bid = treasure / 2
-    bid = find_closest_card(hand, bid)
-    return bid
-
-# Bidding Method 2 - Bid about twice the value of the treasure
-def bid_high(treasure, hand):
-    bid = treasure + treasure 
-    bid = find_closest_card(hand, bid)
-    return bid
-
-# Bidding Method 3 - Random 1-15
-def bid_random(hand):
-    bid = randint(1, 15)
-    bid = find_closest_card(hand, bid)
-    return bid
-
-# Bidding Method 4 - Bid the same as the treasure
-def bid_same(treasure, hand):
-    if treasure < 0:
-        #print "treasure drawn was a negative!"
-        treasure = -treasure
-    bid = find_closest_card(hand, treasure)
-    return bid
-
-# Bidding Method 5 - Bid as low as possible
-def bid_out(treasure, hand):
-    bid = 1
-    bid = find_closest_card(hand, bid)
-    return bid
-
-
-# Computer picks a random AI for bidding 
-def pick_random_ai(treasure, hand):
-    i = randint(1, 5)
-    if i == 1:
-        #print "bid low chosen"
-        computer_ai_bid = bid_low(treasure, hand)
-    elif i == 2:
-        #print "bid high chosen"
-        computer_ai_bid = bid_high(treasure, hand)
-    elif i == 3:
-        #print "bid random chosen"
-        computer_ai_bid = bid_random(hand)
-    elif i == 4:
-        #print "bid same chosen"
-        computer_ai_bid = bid_same(treasure, hand)
-    elif i == 5:
-        #print "bid out chosen"
-        computer_ai_bid = bid_out(treasure, hand)
-    return computer_ai_bid
-
-computer_bidding_methods = [pick_random_ai, pick_random_ai, pick_random_ai, pick_random_ai, pick_random_ai]
-
-def new_computers_turn(computer_bidding_methods, players_hands, treasure):
-    bids = []
-    for bidf, hand in zip(computer_bidding_methods, players_hands):
-        bid = bidf(treasure, hand)
-        bids.append(bid)
-    return bids
-
-def humans_turn(humans, players_hands, treasure):
-    bids = []
-    humans = range(humans)
-    for human in humans:
-        bid = human_input(players_hands[human], treasure)
-        bids.append(bid)
-    return bids
-
-def computers_turn(computers, players_hands, treasure, humans):
-    bids = []
-    computers = range(computers)
-    for computer in computers:
-        bid = pick_random_ai(treasure, players_hands[(computer + humans)])
-        bids.append(bid)
-    return bids
-
 def join_bids(human, computer):
     human.extend(computer)
     bids = human
     return bids
 
-def biding_dict_builder(bids):
+def bidding_dict_builder(bids):
     bid_dict = {}
     for player in range(len(bids)):
         bid = bids[player]
@@ -172,22 +60,48 @@ def biding_dict_builder(bids):
             bid_dict[bid] = [player]
     return bid_dict
 
+def xxdetermine_efficiency(bids, treasure):
+    efficiency = []
+    for i in bids:
+        if i < 0:
+            efficiency.append(round(treasure / float(i), 2))
+        elif i >=0:
+            efficiency.append(round(i / float(treasure), 2))
+    #for i in bids:
+    #    efficiency.append(round(treasure / float(i), 2))
+    return efficiency
+def zzdetermine_efficiency(efficiency, bids, treasure):
+    for i in bids:
+        efficiency.append(treasure - i)
+    return efficiency
+def determine_trick_efficiency(bids, treasure):
+    trick_efficiency = []
+    for i in bids:
+        trick_efficiency.append(treasure - i)
+    return trick_efficiency
+
+def efficiency_tracker(total_efficiency, trick_efficiency):
+    dog = [sum(i) for i in zip(total_efficiency, trick_efficiency)]
+    print "dog", dog
+
+def empty_efficiency_builder(list_of_players):
+    empty = []
+    for i in list_of_players:
+        empty.append(0)
+    print "empty", empty
+    return empty
+
+
+
 def winning_player_finder(bids, treasure):
-    bid_dict = biding_dict_builder(bids)
+    bid_dict = bidding_dict_builder(bids)
     reverse = treasure >= 0
     for bid in sorted(bid_dict, reverse=reverse):
         players = bid_dict[bid]
         if len(players) == 1:
             return players[0]
-    print "No one wins this trick!  Treasure carries over to next round."
+    print "\nNo one wins this trick!  Treasure carries over to next round.", "\n"
     return None
-
-def remove_cards_from_hands(hands, bids):
-    print "bids!!", bids
-    players = range(len(bids))
-    for player in players:
-        hands[player].remove(bids[player])
-    return hands
 
 def trick_winner_scores(winner, treasure, scores):
     if winner == None:
@@ -202,42 +116,194 @@ def game_winner_tie_checker(scores):
     winners = list()
     if number_of_winners > 1:
         winners = [i for i,x in enumerate(scores) if x == max(scores)]
-        print "There are %r winners for this game!" % number_of_winners
+        print "\nThere are %r winners for this game!" % number_of_winners
         return winners
     game_winner = scores.index(max(scores))    
     return game_winner
 
 
+
+# Human
+def humans_turn(humans, players_hands, treasure, treasure_deck, all_bids):
+    bids = []
+    humans = range(humans)
+    for human in humans:
+        bid = human_input(players_hands[human], treasure, treasure_deck, human)
+        bids.append(bid)
+    return bids
+
+def human_input(hand, treasure, treasure_deck, human):
+    bid = 0
+    while bid not in hand:
+        print "\n      Turn: Player %r" % human
+        print "The treasure is: *%r*" % treasure
+        print "Choose from your hand: ", hand
+        print "Treasures   remaining: ", treasure_deck
+        bid = raw_input("\n> ")
+        try:
+            bid = int(bid)
+        except:
+            print "\nWords are not numbers!"
+        if bid not in hand:
+            print "\nTry again!"
+    return bid
+
+# Computer
+def new_computer_turn(computer_bidding_methods, players_hands, treasure, humans):
+    bids = []
+    for i in computer_bidding_methods:
+        bid = i(treasure, players_hands[humans])
+        humans += 1
+        bids.append(bid)
+    return bids
+
+def build_computer_bidding_methods_list(number_of_computers):
+    computer_bidding_methods = []
+    size = range(number_of_computers)
+    for i in size:
+        computer_bidding_methods.append(bid_smart)
+    return computer_bidding_methods
+
+
+
+# Bidding Method 1 - Bid Smart
+def bid_smart(treasure, hand):
+    if treasure <= 0:
+        bid = low_treasure(hand)
+    elif treasure in range(1, 6): 
+        bid = medium_treasure(hand)
+    elif treasure > 5:
+        bid = high_treasure(hand)
+    return bid
+
+def low_treasure(hand):
+    bid = bid_chance()
+    if bid in hand:
+        return bid
+    bid = bid_high()
+    if bid in hand:
+        return bid
+    bid = bid_low()
+    if bid in hand:
+        return bid
+    bid = find_closest_card(hand, bid_chance())
+    return bid
+
+def medium_treasure(hand):
+    bid = bid_chance()
+    if bid in hand:
+        return bid
+    bid = bid_high()
+    if bid in hand:
+        return bid
+    bid = bid_low()
+    if bid in hand:
+        return bid
+    bid = find_closest_card(hand, bid_chance())
+    return bid 
+
+def high_treasure(hand):
+    bid = bid_high()
+    if bid in hand:
+        return bid
+    bid = bid_low()
+    if bid in hand:
+        return bid
+    bid = bid_chance()
+    if bid in hand:
+        return bid
+    bid = find_closest_card(hand, bid_high())
+    return bid 
+
+def bid_low():
+    bid = randint(1, 5)
+    return bid
+def bid_chance():
+    bid = randint(6, 10)
+    return bid
+def bid_high():
+    bid = randint(11, 15)
+    return bid
+
+# Finds the closest card in hand - Defaults to return the lower value if equal distance 
+# **might not be true anymore**
+def find_closest_card(hand, target_bid):
+    for diff in range(40):
+        bid = target_bid + diff
+        if bid in hand:
+            return bid
+        bid = target_bid - diff
+        if bid in hand:
+            return bid
+    print "ERROR 23: Bid out of range!"
+
+
 #############################################
 ###########    M    A    I    N   ###########
 #############################################
+def main():
+    """Raj, a bidding game of perfect information
+    $raj.py [humans] [computers]
+    """
 
-while len(treasure_deck) > 0:
+    games_won = 0
+    args = build_parser()
+    number_of_humans = args.get('humans')
+    number_of_computers = args.get('computers')
+    print "args!", args
+    print number_of_humans
+    print number_of_computers
+    total_players = number_of_humans + number_of_computers
+    list_of_players = range(total_players)
+    players_hands = create_hands(list_of_players)
+    scores = create_scores(list_of_players)
+    treasure_deck = set([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5 ,6, 7, 8, 9, 10])
+    last_round_treasure = None
+    last_round_winner = 0
+    all_bids = []
+    total_efficiency = empty_efficiency_builder(list_of_players)
     
-    # draw the treasure to bid on
-    treasure_drawn = draw_treasure(treasure_deck, last_round_winner, last_round_treasure)
-
-    # Tally all bids for humans and computers
-    humans_bids = humans_turn(number_of_humans, players_hands, treasure_drawn)
-    computers_bids = new_computers_turn(computer_bidding_methods, players_hands, treasure_drawn)
-    #computers_bids = computers_turn(number_of_computers, players_hands, treasure_drawn, number_of_humans)
-    all_bids = join_bids(humans_bids, computers_bids)
-    # Pick the winner of this trick    
-    trick_winner = winning_player_finder(all_bids, treasure_drawn)
-    # Clean up played cards
-    players_hands = remove_cards_from_hands(players_hands, all_bids)
-    # Tally up score
-    scores = trick_winner_scores(trick_winner, treasure_drawn, scores)
-
-    # If no one wins a trick, the treasure carries over
-    last_round_treasure = treasure_drawn
-    last_round_winner = trick_winner
+    ######   SET HOW THE COMPUTER PLAYS   ######
+    computer_bidding_methods = build_computer_bidding_methods_list(number_of_computers)
     
-    print " Trick winner: ", trick_winner
-    print "         bids:", all_bids
-    print "Scores so far:", scores
+    while len(treasure_deck) > 0:
+        # Draw the treasure to bid on
+        treasure_drawn = draw_treasure(treasure_deck, last_round_winner, last_round_treasure)
 
-game_winner = game_winner_tie_checker(scores)
-print "Player(s) %r wins the game!" % game_winner
+        value_of_treasure = round(0.0666666 * (35 - (40 / float(total_players))) * treasure_drawn, 2)
+        #value_of_treasure = round(value_of_treasure, 2)
+        print "value of treasures", value_of_treasure
 
-    
+        # Tally all bids for humans and computers
+        humans_bids = humans_turn(number_of_humans, players_hands, treasure_drawn, treasure_deck, all_bids)
+        computers_bids = new_computer_turn(computer_bidding_methods, players_hands, treasure_drawn, number_of_humans)
+        
+        all_bids = join_bids(humans_bids, computers_bids)
+        # Find the winner of this trick    
+        trick_winner = winning_player_finder(all_bids, treasure_drawn)
+
+        # Find efficiency of all bids per trick
+        trick_efficiency = determine_trick_efficiency(all_bids, treasure_drawn)
+        print "trick efficiency", trick_efficiency
+        efficiency_tracker(total_efficiency, trick_efficiency)
+
+        # Clean up played cards
+        players_hands = remove_cards_from_hands(players_hands, all_bids)
+        # Tally up score
+        scores = trick_winner_scores(trick_winner, treasure_drawn, scores)
+        # If no one wins a trick, the treasure carries over
+        last_round_treasure = treasure_drawn
+        last_round_winner = trick_winner
+        
+        print " Trick winner: <%r>" % trick_winner
+        print " Cards Played: %r" % all_bids
+        print "Scores so far: %r" % scores
+
+    # Game is Finished!
+    game_winner = game_winner_tie_checker(scores)
+    print "\nPlayer(s)      <%r>     wins the game!" % game_winner
+    print " Final Scores: %r\n" % scores
+    games_won += 1
+
+if __name__ == '__main__':
+    main()
